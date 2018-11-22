c4c61b9871165f9d188103a497cfc447cff7c2bf
Misael <41314269+Misael-Chacon@users.noreply.github.com>
2018-11-22 06:30:22 -0600

    Add files via upload

---
+++ public/js/funcionalidad.js
@@ -0,0 +1,35 @@
+//Se cargo todo SELECTOR DE PAICES
+$(document).ready(function(){
+    $.ajax({
+        url: "/paices",
+        method:"GET",
+        dataType:"json",
+        success:function(res){
+            //console.log(res);
+            for (var i=0;i<res.length;i++){
+                $("#pais").append(`<option value="${res[i].codigo_pais}">${res[i].nombre_pais}</option>`);
+            };
+        },
+        error:function(error){
+            console.error(error);
+        }
+    });
+});
+
+$("#registrar").click(function(){
+	var parametros = `nombrecompleto=${$("#nombrecompleto").val()}&nombreusuario=${$("#nombreusuario").val()}&correo=${$("#correo").val()}&password=${$("#password").val()}&genro=${$("#genro").val()}&pais=${$("#pais").val()}`;
+	console.log(parametros);
+	$.ajax({
+		url:"/guardarregistro",
+		method:"POST",
+		data: parametros,
+		dataType:"json", //json
+		success: function(res){ //200 OK
+            console.log(res);
+            window.location.replace("/sesioniniciada.html");
+		},
+		error:function(error){
+			console.error(error);
+		}
+    }); 
+});
