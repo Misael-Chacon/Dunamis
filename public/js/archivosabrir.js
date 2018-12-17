b794a20e177bb3dcffb4b190783270864bebdcc7
Misael <41314269+Misael-Chacon@users.noreply.github.com>
2018-12-17 12:58:28 -0600

    Add files via upload

--- public/js/archivosabrir.js
+++ public/js/archivosabrir.js
@@ -13,20 +13,3 @@ $(document).ready(function(){
     });
 });
 
-
-$(document).ready(function(){
-	$.ajax({
-		url:"/Abierto/:codigo_proyecto",
-		method:"GET",
-		dataType:"json",
-		success:function(res){
-			//console.log(res);
-                $("#editor").append(` ${res[0].nombre_archivo_html}`);
-                $("#editorr").append(` ${res[0].nombre_archivo_css}`);
-                $("#editora").append(` ${res[0].nombre_archivo_js}`);
-		},
-		error:function(error){
-			console.error(error);
-		}
-    });
-});
--- public/js/controlproyectos.js
+++ public/js/controlproyectos.js
@@ -5,27 +5,27 @@ $(document).ready(function(){
         method: "GET",
         datatype: "json",
         success: function (res) {
-            if (res.codigo_usuario == 1) {
+            if (res.plan == 1) {
                 if (res.proyectos == 1) {
                     window.location.href ="/aviso.html";
                 }
                 window.location.href = "/proyectos.html";
 
             } else {
-                if (res.codigo_usuario == 2) {
+                if (res.plan == 2) {
                     if (res.proyectos < 11) {
                         window.location.href = "/proyectos.html";
                     }
                     window.location.href = "/aviso.html";
 
                 } else {
-                    if (res.codigo_usuario == 3) {
+                    if (res.plan == 3) {
                         if (res.proyectos < 26) {
                             window.location.href = "/proyectos.html";
                         }
                         window.location.href = "/aviso.html";
                     } else {
-                        if (res.codigo_usuario == 4) {
+                        if (res.plan == 4) {
                             if (res.proyectos < 51) {
                                 window.location.href = "/proyectos.html";
                             }
