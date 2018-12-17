84bf6b1097e23ddd0b63ddfd974256d60989ba20
Misael <41314269+Misael-Chacon@users.noreply.github.com>
2018-12-17 13:24:44 -0600

    Add files via upload

---
+++ BD/CONSULTAS.17-12-2018.sql
@@ -0,0 +1,11 @@
+select codigo_proyecto from tbl_proyectos where nombre_proyecto = "FORMULARIO"
+SELECT * FROM tbl_proyectos
+SELECT * FROM tbl_usuarios_x_tbl_proyectos
+SELECT * FROM tbl_tipo_usuario
+SELECT codigo_plan from tbl_usuarios WHERE codigo_usuario = 3
+select * from tbl_archivo_js
+select count(codigo_proyecto) as proyectos from tbl_proyectos where codigo_usuario = "5";
+select count(codigo_proyecto) as proyectos, codigo_usuario from tbl_proyectos where codigo_usuario =28
+SELECT nombre_proyecto, descripcion FROM tbl_proyectos
+SELECT nombre_proyecto, descripcion FROM tbl_proyectos WHERE codigo_usuario = 9
+SELECT nombre_proyecto FROM tbl_proyectos WHERE codigo_usuario = 7 and codigo_proyecto = 6
